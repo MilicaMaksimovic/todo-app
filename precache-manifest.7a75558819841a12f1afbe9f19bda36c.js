da8bc5af1fc152c163d95ac310862db877b5ba3c
mmilica <milica.maksimovic@datadesign.me>
2020-01-21 11:25:02 +0100

    Updates

---
+++ precache-manifest.7a75558819841a12f1afbe9f19bda36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2cf94ad51c3fea8657e1804e296072",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "3991b07110cd9b93370b",
+    "url": "/todo-app/static/css/main.b1d2af55.chunk.css"
+  },
+  {
+    "revision": "5811bf7fda1f984b8e3e",
+    "url": "/todo-app/static/js/2.1210d4e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app/static/js/2.1210d4e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3991b07110cd9b93370b",
+    "url": "/todo-app/static/js/main.d842ea12.chunk.js"
+  },
+  {
+    "revision": "cbc794fa89b11161d195",
+    "url": "/todo-app/static/js/runtime-main.c846c3d1.js"
+  }
+]);
